b43eb47dae8f57b51249fa8f068685ef5af4ff0e
Ziyad <kakangtea74@gmail.com>
2024-06-12 10:00:26 +0700

    Menambahkan Riwayat ke dalam masing-masing model

--- src/libs/services/M_Akun.js
+++ src/libs/services/M_Akun.js
@@ -3,6 +3,7 @@
 import { cookies } from "next/headers"
 import { cookie_set } from "../functions/cookie"
 import { api_delete, api_get, api_post, api_put } from "../functions/web_service"
+import { M_Riwayat_log } from "./M_Riwayat"
 
 export const M_Akun_logout = async () => {
     if(cookies().has('userdata')) {
@@ -22,7 +23,6 @@ export const M_Akun_getAll = async () => {
 
 export const M_Akun_login = async (email_akun, password_akun, duration) => {
     const response = await api_post('/v1/userdata/create', {email_akun, password_akun})
-    console.log(response)
 
     if(response.success) {
         cookies().set('userdata', response.data.data)
@@ -38,6 +38,8 @@ export const M_Akun_login = async (email_akun, password_akun, duration) => {
 export const M_Akun_create = async (payload) => {
     const response = await api_post('/v1/data/akun', payload)
 
+    await M_Riwayat_log('Tambah', `Menambahkan ${Array.isArray(payload) ? payload.length : '1'} Data ke dalam Data Akun`, payload)
+
     return {
         success: response.success,
         message: response.message,
@@ -48,6 +50,8 @@ export const M_Akun_create = async (payload) => {
 export const M_Akun_update = async (id_akun, payload) => {
     const response = await api_put('/v1/data/akun', {id_akun, payload})
 
+    await M_Riwayat_log('Ubah', `Mengubah ${Array.isArray(id_akun) ? id_akun.length : '1'} Data dari Data Akun`, {id_akun, payload})
+
     return {
         success: response.success,
         message: response.message,
@@ -57,6 +61,8 @@ export const M_Akun_update = async (id_akun, payload) => {
 
 export const M_Akun_delete = async (id_akun) => {
     const response = await api_delete('/v1/data/akun', {id_akun})
+
+    await M_Riwayat_log('Hapus', `Menghapus ${Array.isArray(id_akun) ? id_akun.length : '1'} Data dari Data Akun`, {id_akun})
 
     return {
         success: response.success,
--- src/libs/services/M_Surat.js
+++ src/libs/services/M_Surat.js
@@ -15,6 +15,8 @@ export const M_Surat_getAll = async () => {
 export const M_Surat_create = async (payload) => {
     const response = await api_post('/v1/data/surat', payload)
 
+    await M_Riwayat_log('Tambah', `Menambahkan ${Array.isArray(payload) ? payload.length : '1'} Data ke dalam Data Surat`, payload)
+
     return {
         success: response.success,
         message: response.message,
@@ -25,6 +27,8 @@ export const M_Surat_create = async (payload) => {
 export const M_Surat_update = async (id_surat_izin, payload) => {
     const response = await api_put('/v1/data/surat', {id_surat_izin, payload})
 
+    await M_Riwayat_log('Ubah', `Mengubah ${Array.isArray(id_surat_izin) ? id_surat_izin.length : '1'} Data dari Data Surat`, {id_surat_izin, payload})
+
     return {
         success: response.success,
         message: response.message,
@@ -34,6 +38,8 @@ export const M_Surat_update = async (id_surat_izin, payload) => {
 
 export const M_Surat_delete = async (id_surat_izin) => {
     const response = await api_delete('/v1/data/surat', {id_surat_izin})
+
+    await M_Riwayat_log('Hapus', `Menghapus ${Array.isArray(id_surat_izin) ? id_surat_izin.length : '1'} Data dari Data Surat`, {id_surat_izin})
 
     return {
         success: response.success,
