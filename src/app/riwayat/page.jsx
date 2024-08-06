6c4a447d58f2ce75783e381948f62f17b6308311
Ziyad <kakangtea74@gmail.com>
2024-08-06 08:57:00 +0700

    fixing riwayat page

--- src/app/riwayat/page.jsx
+++ src/app/riwayat/page.jsx
@@ -101,22 +101,24 @@ export default function RiwayatPage() {
                                 </div>
                                 <div className="flex items-center col-span-5 lg:col-span-2 justify-center md:col-span-3">
                                     {value['json'] !== null && (
+                                        <>
                                         <button type="button" onClick={() => document.getElementById(`info_data_${index}`).showModal()} className="px-3 py-2 rounded-full bg-zinc-100 hover:bg-zinc-200 dark:bg-zinc-800 dark:hover:bg-zinc-700 flex items-center justify-center gap-3 w-fit">
                                             <FontAwesomeIcon icon={faSearch} className="w-3 h-3 text-inherit" />
                                             Lihat Data
                                         </button>
+                                        <dialog id={`info_data_${index}`} className="modal">
+                                            <div className="modal-box rounded-md dark:bg-zinc-900 border dark:border-zinc-700">
+                                                <form method="dialog">
+                                                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                                                </form>
+                                                <hr className="my-2 opacity-0" />
+                                                <textarea readOnly rows={30} value={prettyJs(JSON.parse(value['json']))} className="border p-3 w-full bg-transparent dark:border-zinc-700 rounded-md outline-none dark:text-white">
+                                                    
+                                                </textarea>
+                                            </div>
+                                        </dialog>
+                                        </>
                                     )}
-                                    <dialog id={`info_data_${index}`} className="modal">
-                                        <div className="modal-box rounded-md dark:bg-zinc-900 border dark:border-zinc-700">
-                                            <form method="dialog">
-                                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                                            </form>
-                                            <hr className="my-2 opacity-0" />
-                                            <textarea readOnly rows={30} value={prettyJs(value['json'])} className="border p-3 w-full bg-transparent dark:border-zinc-700 rounded-md outline-none dark:text-white">
-                                                
-                                            </textarea>
-                                        </div>
-                                    </dialog>
                                 </div>
                             </div>
                         ))}
